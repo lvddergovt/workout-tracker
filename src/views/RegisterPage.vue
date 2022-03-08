f50afabef99065dd5d48530f83cccb3983578194
Guido <gbakkes@gmail.com>
2022-03-08 22:48:10 +0100

    Add supabase registration and login

--- src/views/RegisterPage.vue
+++ src/views/RegisterPage.vue
@@ -8,7 +8,7 @@
     </div>
 
     <!-- Registration -->
-    <form class="p-8 flex flex-col bg-light-grey rounded-md shadow-lg">
+    <form @submit.prevent="register" class="p-8 flex flex-col bg-light-grey rounded-md shadow-lg">
       <h1 class="text-3xl text-at-light-green mb-4">Register</h1>
 
       <div class="flex flex-col mb-2">
@@ -18,12 +18,12 @@
 
       <div class="flex flex-col mb-2">
         <label for="password" class="mb-1 text-sm text-at-light-green">Password</label>
-        <input type="text" required class="p-2 text-gray-500 focus:outline-none" id="password" v-model="password" />
+        <input type="password" required class="p-2 text-gray-500 focus:outline-none" id="password" v-model="password" />
       </div>
 
       <div class="flex flex-col mb-2">
         <label for="confirmPassword" class="mb-1 text-sm text-at-light-green">Confirm password</label>
-        <input type="text" required class="p-2 text-gray-500 focus:outline-none" id="confirmPassword" v-model="confirmPassword" />
+        <input type="password" required class="p-2 text-gray-500 focus:outline-none" id="confirmPassword" v-model="confirmPassword" />
       </div>
 
       <button type="submit" class="mt-6 py-2 px-6 rounded-sm self-start text-sm text-white bg-at-light-green duration-200 border-solid border-2 border-transparent hover:border-at-light-green hover:bg-white hover:text-at-light-green">
@@ -40,6 +40,8 @@
 
 <script>
 import { ref } from "vue";
+import { supabase } from "../supabase/init";
+import { useRouter } from "vue-router";
 
 export default {
   name: "register",
@@ -47,18 +49,45 @@ export default {
   setup() {
 
     // create data / vars
+    const router = useRouter();
     const email = ref(null);
     const password = ref(null);
     const confirmPassword = ref(null);
     const errorMsg = ref(null);
 
-    
+    // Register new user
+    const register = async() => {
+      if (password.value === confirmPassword.value) {
+        try {
+          const { error } = await supabase.auth.signUp({
+            email: email.value,
+            password: password.value
+          });
+
+          if (error) throw error;
+          router.push({ name: "Login" })
+        } catch(error) {
+          errorMsg.value = error.message;
+          setTimeout(() => {
+            errorMsg.value = null;
+          }, 5000);
+        }
+
+        return;
+      }
+
+      errorMsg.value = "Error: Passwords do not match";
+      setTimeout(() => {
+        errorMsg.value = null;
+      }, 5000);
+    }
 
     return {
       email,
       password,
       confirmPassword,
-      errorMsg
+      errorMsg,
+      register
     };
 
   }
